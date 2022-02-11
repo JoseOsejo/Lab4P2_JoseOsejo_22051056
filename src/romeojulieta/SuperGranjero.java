7aa92e2e980588dd4e2c5369d257902018d22710
Jose Carlos Osejo <jcoq2003@gmail.com>
2022-02-11 17:14:22 -0600

    Super Granjero

--- src/romeojulieta/SuperGranjero.java
+++ src/romeojulieta/SuperGranjero.java
@@ -9,6 +9,32 @@ package romeojulieta;
  *
  * @author jcoq2
  */
-public class SuperGranjero {
+public class SuperGranjero extends Aldeano{
+    private double puntosAtaque;
+
+    public SuperGranjero(double puntosAtaque, String nombre, String apellido, int edad, double puntosVida) {
+        super(nombre, apellido, edad, 1000);
+        this.puntosAtaque = 1000;
+    }
+
+    public double getPuntosAtaque() {
+        return puntosAtaque;
+    }
+
+    public void setPuntosAtaque(double puntosAtaque) {
+        this.puntosAtaque = puntosAtaque;
+    }
+
+    @Override
+    public double ataque(Aldeano atacar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return "SuperGranjero{" + "puntosAtaque=" + puntosAtaque + '}';
+    }
+
+    
     
 }
